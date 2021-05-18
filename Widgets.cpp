c6e1a14b3b085de5f4700299d0168ca1e4ebed9a
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-05-17 20:27:12 -0500

    Add files via upload

---
+++ Widgets.cpp
@@ -0,0 +1,61 @@
+/*
+Widgets
+Name
+Date
+This program uses a class to represent products
+sold by Fidget Widget Inc. You must add the
+code to create another product with the following
+information...
+Product Name – Avocado Eraser
+Unit Cost - $1.50
+MSRP - $3.00
+Minimum Order Quantity – 10
+*/
+#include <iostream>
+using namespace std;
+
+/*
+Stores the product name, unit cost, MSRP, and minimum
+order quantity for a product sold by Fidget Widget Inc.
+*/
+class Widget
+{
+public:
+    string productName;
+    double unitCost;
+    double msrp;
+    int minOrderQuantity;
+};
+
+/*
+Prints the widget's information to the console.
+*/
+void printWidgetInfo(Widget w)
+{
+    cout << "Widget Name: " << w.productName << "\n";
+    cout << "Widget Unit Cost: $" << w.unitCost << "\n";
+    cout << "Widget MSRP: $" << w.msrp << "\n";
+    cout << "Widget Min Order: " << w.minOrderQuantity << "\n";
+}
+
+int main()
+{
+    Widget w1;
+    w1.productName = "Pusheen Post-Its";
+    w1.unitCost = 1.00;
+    w1.msrp = 1.75;
+    w1.minOrderQuantity = 25;
+    printWidgetInfo(w1);
+    cout << "\n";
+
+    Widget w2;
+    w1.productName = "Rubik's Cube";
+    w1.unitCost = 4.00;
+    w1.msrp = 12.00;
+    w1.minOrderQuantity = 15;
+    printWidgetInfo(w2);
+    cout << "\n";
+
+    // add your code here!
+    return 0;
+}
